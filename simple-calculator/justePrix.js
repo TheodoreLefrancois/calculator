ff1ca9e102dc05d7a2ab4c498f0d320b465ee58e
tlefrancois <theodore.lefrancois2906@gmail.com>
2020-09-24 09:28:27 +0200

    Separating justePrix

---
+++ simple-calculator/justePrix.js
@@ -0,0 +1,15 @@
+function justePrix(name, vincentLagaffe) {
+    let choix = 0
+
+        while (choix != random) {
+            if (random>choix ){
+                console.log("C'est plus !");
+            }
+            else {
+                console.log("C'est moins !");
+            }
+            choix = +prompt("Veuillez entrer un nombre compris entre 1 et 100");
+        }
+        console.log(`Well done ${name} !!!`)
+
+    }
